c42fbaf4cc81f707941e0cda7a4503d2206e039c
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-29 23:43:31 +0900

    feat(userboard): add delete logic to service

--- src/main/java/com/example/sudoku_app_api/repository/LogRepository.java
+++ src/main/java/com/example/sudoku_app_api/repository/LogRepository.java
@@ -1,9 +1,18 @@
 package com.example.sudoku_app_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.sudoku_app_api.entity.Log;
+import com.example.sudoku_app_api.entity.UserBoard;
 
-public interface LogRepository extends JpaRepository<Log,Long>{
-    
+public interface LogRepository extends JpaRepository<Log, Long> {
+    /**
+     * UserBoardId(uid,boardId)に紐づくすべてのログを削除する
+     */
+    @Modifying
+    @Query("DELETE FROM Log l WHERE l.userBoard.id = :id")
+    void deleteByUserBoardId(@Param("id") UserBoard.UserBoardId id);
 }
--- src/main/java/com/example/sudoku_app_api/service/UserBoardService.java
+++ src/main/java/com/example/sudoku_app_api/service/UserBoardService.java
@@ -9,6 +9,7 @@ import com.example.sudoku_app_api.entity.Board;
 import com.example.sudoku_app_api.entity.User;
 import com.example.sudoku_app_api.entity.UserBoard;
 import com.example.sudoku_app_api.repository.BoardQueryRepository;
+import com.example.sudoku_app_api.repository.LogRepository;
 import com.example.sudoku_app_api.repository.UserBoardQueryRepository;
 import com.example.sudoku_app_api.repository.UserBoardRepository;
 
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class UserBoardService {
 
+    private final LogRepository logRepository;
     private final UserService userService;
     private final BoardQueryRepository bQueryRepository;
     private final UserBoardRepository ubRepository;
@@ -35,10 +37,20 @@ public class UserBoardService {
                 .orElseThrow(() -> new EntityNotFoundException("ボードが見つかりません。ID: " + boardId));
         String uid = user.getUid();
         if (existsTooManyUserBoardInProgress(uid)) {
-            throw new IllegalArgumentException("同時に取り組める数独は"+sudokuGameProperties.getMaxInProgress()+"つまでです。");
+            throw new IllegalArgumentException("同時に取り組める数独は" + sudokuGameProperties.getMaxInProgress() + "つまでです。");
         }
         UserBoard ub = UserBoard.create(user, board);
         ubRepository.save(ub);
+    }
+
+    @Transactional
+    public void deleteBoardByUser(
+            Jwt jwt, Long boardId) {
+        User user = userService.getOrCreateUser(jwt);
+        UserBoard.UserBoardId id = UserBoard.UserBoardId.create(user.getUid(), boardId);
+
+        logRepository.deleteByUserBoardId(id);
+        ubRepository.deleteById(id);
     }
 
     private boolean existsTooManyUserBoardInProgress(String uid) {
