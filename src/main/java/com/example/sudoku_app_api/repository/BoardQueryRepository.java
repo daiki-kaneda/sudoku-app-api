ef9ca3b267e1c27278d4e774154bcc551ec4285f
daiki-kaneda <daiki0622developing@gmail.com>
2026-01-02 14:36:05 +0900

    fix(board): modify jpql of findByName

--- src/main/java/com/example/sudoku_app_api/repository/BoardQueryRepository.java
+++ src/main/java/com/example/sudoku_app_api/repository/BoardQueryRepository.java
@@ -2,6 +2,7 @@ package com.example.sudoku_app_api.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import com.example.sudoku_app_api.entity.Board;
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 
 public interface BoardQueryRepository extends Repository<Board, Long> {
+    @Query("SELECT b FROM Board b WHERE b.name LIKE %:name%")
     Page<Board> findByName(String name, Pageable pageable);
     Optional<Board> findById(Long id);
 }
